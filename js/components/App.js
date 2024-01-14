dfe4f945752d6f933361457c526890f3ef04a847
Dejan1990 <dejan.simo@hotmail.com>
2024-01-14 11:36:03 +0100

    Bring it all together - starting point

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,53 @@
-import AppButton from './AppButton.js';
-
 export default {
-    components: {
-        'app-button': AppButton
-    }
+    template: `
+    <section v-show="inProgressAssignments.length">
+    <h2 class="font-bold mb-2">In Progress</h2>
+    <ul>
+        <li 
+            v-for="assignment in inProgressAssignments" 
+            :key="assignment.id"
+        >
+            <label>
+                {{ assignment.name }}
+                <input type="checkbox" v-model="assignment.complete">
+            </label>
+        </li>
+    </ul>
+</section>
+
+<section v-show="completedAssignments.length" class="mt-8">
+    <h2 class="font-bold mb-2">Completed</h2>
+    <ul>
+        <li 
+            v-for="assignment in completedAssignments" 
+            :key="assignment.id"
+        >
+            <label>
+                {{ assignment.name }}
+                <input type="checkbox" v-model="assignment.complete">
+            </label>
+        </li>
+    </ul>
+</section>
+    `,
+
+    data() {
+        return {
+            assignments: [
+                { name: 'Read chapter 4', complete: true, id: 1 },
+                { name: 'Finish Project', complete: false, id: 2 },
+                { name: 'Turn in homework', complete: false, id: 3 }
+            ]
+        }
+    },
+
+    computed: { // sort of like methods that can be cashed
+        inProgressAssignments() {
+            return this.assignments.filter(assignment => !assignment.complete)
+        },
+
+        completedAssignments() {
+            return this.assignments.filter(assignment => assignment.complete)
+        }
+    },
 }
