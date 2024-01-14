0b4f3a1e8d1370ac3ff0b90964ee1d63b91c1473
Dejan1990 <dejan.simo@hotmail.com>
2024-01-14 10:30:23 +0100

    Props finished

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -6,6 +6,7 @@ export default {
                 'bg-blue-500 hover:bg-blue-700': type === 'primary',
                 'bg-purple-500 hover:bg-purple-700': type === 'secondary',
                 'bg-gray-500 hover:bg-gray-700': type === 'muted',
+                'is-loading': processing
             }" 
             :disabled="processing"
         >
@@ -17,12 +18,13 @@ export default {
         type: { // this type is the name of the prop
             type: String, // this type is the type of the prop
             default: 'primary'
+        },
+
+        processing: {
+            type: Boolean,
+            default: false
         }
     },
 
-    data() {
-        return {
-            processing: true
-        }
-    },
+    
 }
