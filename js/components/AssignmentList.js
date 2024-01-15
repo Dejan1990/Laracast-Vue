85baa3a7dd0d73ecfb13ed8412ae43e15588b122
Dejan1990 <dejan.simo@hotmail.com>
2024-01-15 17:32:23 +0100

    Style change

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -4,16 +4,20 @@ import AssignmentTags from "./AssignmentTags.js";
 export default {
     components: { Assignment, AssignmentTags },
     template: `
-        <section v-show="assignments.length" class="w-70">
-            <h2 class="font-bold mb-2">
-                {{ title }}
-                <span>({{ assignments.length }})</span>
+        <section v-show="assignments.length" class="w-60">
+            <div class="flex justify-between items-start">
+                <h2 class="font-bold mb-2">
+                    {{ title }}
+                    <span>({{ assignments.length }})</span>
+                </h2>
+                <button>&times;</button>
+            </div>
 
-                <assignment-tags 
-                    v-model:currentTag="currentTag"
-                    :initialTags="assignments.map(a => a.tag)"
-                />
-            </h2>
+            <assignment-tags 
+                v-model:currentTag="currentTag"
+                :initialTags="assignments.map(a => a.tag)"
+            />
+
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment 
                     v-for="assignment in filteredAssignments"
