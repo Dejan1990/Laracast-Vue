f0b036a9e7fe0c4eae4fa8d83b46a79a3fc0b692
Dejan1990 <dejan.simo@hotmail.com>
2024-01-15 07:42:20 +0100

    Add component for form

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,18 +1,14 @@
 import AssignmentList from "./AssignmentList.js"
+import AssignmentCreate from "./AssignmentCreate.js"
 
 export default {
-    components: { AssignmentList },
+    components: { AssignmentList, AssignmentCreate },
     template: `
         <section class="space-y-6">
             <assignment-list title="In Progress" :assignments="filters.inProgress"></assignment-list>
             <assignment-list title="Completed" :assignments="filters.completed"></assignment-list>
 
-            <form @submit.prevent="add">
-                <div class="border border-gray-600 text-black">
-                    <input v-model="newAssignment" placeholder="New Assignment..." class="p-2" />
-                    <button type="submit" class="bg-white p-2 border-l">Add</button>
-                </div>
-            </form>
+            <assignment-create :assignments="assignments"></assignment-create>
         </section>
     `,
 
@@ -23,8 +19,6 @@ export default {
                 { name: 'Finish Project', complete: false, id: 2 },
                 { name: 'Turn in homework', complete: false, id: 3 }
             ],
-
-            newAssignment: ''
         }
     },
 
@@ -34,18 +28,6 @@ export default {
                 inProgress: this.assignments.filter(assignment => !assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete)
             }
-        }
-    },
-
-    methods: {
-        add() {
-            this.assignments.push({
-                name: this.newAssignment,
-                complete: false,
-                id: this.assignments.length + 1
-            });
-
-            this.newAssignment = '';
         }
     },
 }
